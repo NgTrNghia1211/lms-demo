2e7b753bec84c69ae39f17d14de6750e2279b4c9
NgTrNghia1211 <luckilyislife@gmail.com>
2023-11-14 15:16:43 +0700

    work teacher create page

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,11 +1,109 @@
 "use client"
 
-import React from 'react'
+import React from 'react';
+import * as z from 'zod';
+import axios from 'axios'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link'
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem
+} from '@/components/ui/form'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import toast from 'react-hot-toast';
+
+import './styles.css';
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Title is Required'
+  })
+})
+
 
 const CreatePage = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: ""
+    }
+  })
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post('/api/course', values);
+      router.push(`/teacher/courses/${response.data.id}`)
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
+  }
+
   return (
-    <div>
-        
+    <div className='create-page-container md:items-center md:justify-center'>
+      <div>
+        <h1 className='text-2xl'>Name your course</h1>
+        <p className='text-sm text-slate-600'>What would you like to name your course? Don't worry, you can change this later.</p>
+        <Form {...form}>
+          <form 
+            action=""
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-8'
+          >
+            <FormField
+              control={form.control}
+              name='title'
+              render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Course Title</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isSubmitting}
+                        placeholder="e.g. 'Advanced web development'"
+                        {...field}
+                      >
+                      </Input>
+                    </FormControl>
+                    <FormDescription>
+                      What will you teach in this course?
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }
+            />
+            <div className='buttons-container gap-x-2'>
+              <Link href={'/'}>
+                <Button
+                  type='button'
+                  variant={'ghost'}
+                >
+                  Cancel
+                </Button>
+              </Link>
+
+              <Button
+                type='submit'
+                disabled={isSubmitting || !isValid}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   )
 }
